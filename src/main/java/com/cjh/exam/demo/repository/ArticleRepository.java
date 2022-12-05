1fde4e2b5c5f93b97eacf80d8b3918c8945134c4
devCJH <http-@naver.com>
2022-12-05 20:43:40 +0900

    SQL을 xml파일로 이전, articleModify시에 입력된 정보만 수정될 수 있도록

--- src/main/java/com/cjh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/cjh/exam/demo/repository/ArticleRepository.java
@@ -2,38 +2,22 @@ package com.cjh.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.cjh.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 
-	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?;
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
 	public void writeArticle(String title, String body);
 
-	// SELECT * FROM article WHERE id = ?;
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(int id);
 
-	// SELECT * FROM article ORDER BY id DESC;
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	// DELETE FROM article WHERE id = ?;
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(int id);
 
-	// UPDATE article SET updateDate = NOW(), title = ?, `body` = ? WHERE id = ?;
-	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 
-	// SELECT LAST_INSERT_ID();
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 }
